785b4a7402e474bd34d5b4f93b93dafbecc53524
nek <nek@srez.org>
2015-07-24 18:25:34 +0300

    Fix dev config

--- protected/config/dev.php
+++ protected/config/dev.php
@@ -1,3 +1,10 @@
 <?php
 
-require_once(dirname(__FILE__).'/production.php');
+return CMap::mergeArray(
+                require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'components.php', CMap::mergeArray(
+                        require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'database.php', CMap::mergeArray(
+                                require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'general.php', CMap::mergeArray(
+                                        require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'params.php', require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'modules.php')
+                        )
+                )
+);
